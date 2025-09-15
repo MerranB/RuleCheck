2a9e484554daef746298bc0817e11ca12653e6df
jason <jason.merran.brisbois@gmail.com>
2025-09-15 12:57:19 -0400

    test: configure pytest with TestClient and add sample tests

---
+++ tests/api/test_rules.py
@@ -0,0 +1,4 @@
+def test_rule_endpoint(client):
+    response = client.get("/rulecheck/rules/")
+    assert response.status_code == 200
+    assert "Rules endpoint ready" in response.json()["message"]
